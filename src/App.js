750dbee892aea00e0945c8658ffd3a0a6bdae60e
Brayam Mazuera <brayanmazuera1@gmail.com>
2024-06-24 17:07:25 +0800

    working with promp

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import './App.css';
 import { TodoSearch } from './components/TodoSearch';
 import { TodoList } from './components/TodoList';
 import { TodoItem } from './components/TodoItem';
+import { CreateTodoButton } from './components/CreateTodoButton';
+import React from 'react';
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el Curso de Intro a React.js', completed: false },
+  { text: 'Llorar con la Llorona', completed: false },
+  { text: 'LALALALALA', completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <TodoCounter/>
+    <React.Fragment>
+      <TodoCounter completed={16} total={25} />
       <TodoSearch/>
       <TodoList>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
+        {defaultTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
-     
-    </div>
+      <CreateTodoButton/>
+    </React.Fragment>
   );
 }
 
--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -1,10 +1,10 @@
 
-function TodoCounter() {
-    return (
-      <h1>
-        Has completado 3 de 5 TODOS
-      </h1>
-    );
-  }
-  
-  export { TodoCounter };
+function TodoCounter({ total, completed }) {
+  return (
+    <h1>
+      Has completado {completed} de {total} TODOS
+    </h1>
+  );
+}
+
+export { TodoCounter };
