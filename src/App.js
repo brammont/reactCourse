a1507e864172501291f893becd56ac2140c1c968
Brayam Mazuera <brayanmazuera1@gmail.com>
2024-06-24 15:43:36 +0800

    create todoitem component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <TodoItem />
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,5 +23,13 @@ function App() {
     </div>
   );
 }
-
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
+  );
+}
 export default App;
