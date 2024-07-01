923a1a7263ea026cd8259c7f0c7e478671367d4d
Brayam Mazuera <brayanmazuera1@gmail.com>
2024-07-01 21:20:49 +0800

    delete items and completed all of them

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
     todo => !!todo.completed
   ).length;
   const totalTodos = todos.length;
+
   const searchedTodos = todos.filter(
     (todo) => {
       const todoText = todo.text.toLowerCase();
@@ -29,10 +30,27 @@ function App() {
     }
   );
 
-  console.log('Los usuarios buscan todos de ' + searchValue);
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
+  
   return (
-    <React.Fragment>
-       <TodoCounter
+    <>
+      <TodoCounter
         completed={completedTodos}
         total={totalTodos} 
       />
@@ -40,17 +58,21 @@ function App() {
         searchValue={searchValue}
         setSearchValue={setSearchValue}
       />
+
       <TodoList>
         {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
-      <CreateTodoButton/>
-    </React.Fragment>
+      
+      <CreateTodoButton />
+    </>
   );
 }
 
