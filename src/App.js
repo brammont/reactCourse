3ea23a69e58677cf00495b4f40557fd224a8a3a4
Brayam Mazuera <brayanmazuera1@gmail.com>
2024-07-01 21:09:14 +0800

    adding searching interactive with states way

--- src/App.js
+++ src/App.js
@@ -21,6 +21,13 @@ function App() {
     todo => !!todo.completed
   ).length;
   const totalTodos = todos.length;
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    }
+  );
 
   console.log('Los usuarios buscan todos de ' + searchValue);
   return (
@@ -34,7 +41,7 @@ function App() {
         setSearchValue={setSearchValue}
       />
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,11 @@
-import './TodoList.css'
+import './TodoList.css';
 
 function TodoList({ children }) {
-    return (
-      <ul>
-        {children}
-      </ul>
-    );
-  }
-  
-  export { TodoList };
+  return (
+    <ul className="TodoList">
+      {children}
+    </ul>
+  );
+}
+
+export { TodoList };
